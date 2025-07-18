007e6dba471f4b4219ad6f70fe1f61390531bc53
charinrat2012 <113957159+charinrat2012@users.noreply.github.com>
2025-07-18 09:20:24 +0700

    Update salary_detail_binding.dart

--- lib/app/bindings/salary_detail_binding.dart
+++ lib/app/bindings/salary_detail_binding.dart
@@ -5,7 +5,7 @@ import '../ui/pages/salary_detail_page/salary_dedail_controller.dart';
 class SalaryDetailBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SalaryDedailController>(() => SalaryDedailController());
-    Get.put<SalaryDedailController>(SalaryDedailController());
+    Get.lazyPut<SalaryDetailController>(() => SalaryDetailController());
+    Get.put<SalaryDetailController>(SalaryDetailController());
   }
 }
